0243ec4263d8283f93a6824a072c437c1f6dc5c8
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-10-13 14:09:40 +0530

    dashboard added

--- frontend/src/Components/AllRoute.jsx
+++ frontend/src/Components/AllRoute.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from '../Pages/Dashboard'
 
 const AllRoute = () => {
   return (
     <div>
-      
+        <Routes>
+            <Route path='/' element={<Dashboard/>}/>
+        </Routes>
     </div>
   )
 }
--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Dashboard = () => {
+    const [data, setData]=useState();
+    const [loading, setLoading]=useState(false);
+    const [error , setError]=useState(false);
+
+    useEffect(()=>{
+        setLoading(true)
+        axios.get("http://localhost:5000/users")
+        .then((res)=>setData(res.data),setLoading(false))
+        .catch((err)=>console.log(err),setError(true))        
+    },[])
+    console.log(data)
   return (
     <div>
+        {
+            data?.data.map((e)=><div key={e.id}>{e.title}</div>)
+        }
+
       
     </div>
   )
