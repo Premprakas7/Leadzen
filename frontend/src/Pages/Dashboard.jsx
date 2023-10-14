2f8e319e5412700417d906fa21c72ae935646610
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-10-14 10:26:54 +0530

    updated

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import "./Dashboard.css"
 
 const Dashboard = () => {
-    const [data, setData]=useState();
+    const [data, setData]=useState([]);
     const [loading, setLoading]=useState(false);
     const [error , setError]=useState(false);
 
@@ -13,10 +13,28 @@ const Dashboard = () => {
         .then((res)=>setData(res.data),setLoading(false))
         .catch((err)=>console.log(err),setError(true))        
     },[])
-    console.log(data)
+    
+   
+    const sortHandler=(e)=>{
+         let value=e.target.value;
+         let by={...data};
+            if(value==="asc"){ by?.data.sort((a,b)=>{ return a.price-b.price })
+            }
+            else if(value==="desc"){ by?.data.sort((a,b)=>{ return b.price-a.price })
+            }
+            setData(by);  
+    }
   return (
         <div >
             <h1>Dashboard</h1>
+            <div>
+                <p>Sort Price</p>
+                <select onClick={sortHandler}>
+                    <option value=""></option>
+                    <option value="desc">High To Low</option>
+                    <option value="asc">Low To High</option>
+                </select>
+            </div>
     
     <div className='box' >
         {
